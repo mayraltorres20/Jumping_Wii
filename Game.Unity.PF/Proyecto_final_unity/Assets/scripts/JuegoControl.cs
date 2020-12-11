b380ca175ceb9c632dd35facae65e3abe456c858
mayraltorres20 <66183694+mayraltorres20@users.noreply.github.com>
2020-12-10 20:37:43 -0600

    Juego Final

---
+++ Game.Unity.PF/Proyecto_final_unity/Assets/scripts/JuegoControl.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public enum GameState{Idle, Playing, Fin, Replay};
+
+public class JuegoControl : MonoBehaviour{
+
+    [Range(0f, 0.20f)]
+    public float parallaxSpeed = 0.02f;
+    public RawImage background;
+    public RawImage platform;
+    public GameObject uiIdle;
+    public GameObject uiScore;
+    public Text pointsText;
+    public Text recordText;
+    public GameState gameState = GameState.Idle;
+    public GameObject uiIdel;
+    
+    
+    //public Text recordText;
+       
+    public GameObject Player;
+    public GameObject enemyGenerator;
+
+    public float scaleTime = 6f;
+    public float scaleInc = .25f;
+    
+    private AudioSource musicPlayer;
+    private int points = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       musicPlayer = GetComponent<AudioSource>();
+       recordText.text = "BEST: " + GetMaxScore().ToString(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   
+        bool userAction = Input.GetKeyDown("up") || Input.GetMouseButtonDown(0);
+
+        //comienza el juego
+        if(gameState == GameState.Idle && userAction){
+            gameState = GameState.Playing;
+            uiIdle.SetActive(false);
+            uiScore.SetActive(true);
+            uiIdle.SetActive(false);
+            Player.SendMessage("UpdateState", "Player_run");
+            Player.SendMessage("DustPlay");
+            enemyGenerator.SendMessage("StartGenerator");
+            musicPlayer.Play();
+            InvokeRepeating("GameScale", scaleTime, scaleTime);
+        }
+
+        //Juego corriendo
+        else if(gameState == GameState.Playing){
+            Parallax();
+        } 
+        //Juego preparado para reinicio
+        else if(gameState == GameState.Replay){
+            if(userAction){
+                RestartGame();
+            }
+        } 
+    }
+    //movimiento paisaje
+    void Parallax()
+    {        
+            float finalSpeed = parallaxSpeed * Time.deltaTime;
+            background.uvRect = new Rect(background.uvRect.x + finalSpeed, 0f, 1f, 1f);
+            platform.uvRect = new Rect(platform.uvRect.x + finalSpeed * 4, 0f, 1f, 1f); 
+    }
+
+    //reinicio juego puntos
+    public void RestartGame(){
+        ResetTimeScale();
+        SceneManager.LoadScene("Principal");
+    }
+
+    void GameScale(){
+        Time.timeScale += scaleInc;
+        Debug.Log("Ritmo incrementado:" + Time.timeScale.ToString());
+    }
+    
+    public void ResetTimeScale(float newTimeScale = 1f){
+        CancelInvoke("GameTimeScale");
+        Time.timeScale = newTimeScale;
+        Debug.Log("Ritmo restablecido:" + Time.timeScale.ToString());
+    }
+
+    public void IncreasePoints(){
+        pointsText.text = (++points).ToString();
+        if(points >= GetMaxScore()){
+            recordText.text = "BEST: " + points.ToString();
+            SaveScore(points);
+        }
+    }
+
+    public int GetMaxScore(){
+        return PlayerPrefs.GetInt("Max Points", 0);
+    }
+
+    public void SaveScore(int currentPoints){
+        PlayerPrefs.SetInt("Max Points", currentPoints);
+    }
+}
---
+++ Game.Unity.PF/Proyecto_final_unity/Assets/scripts/PlayerControl.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerControl : MonoBehaviour
+{
+    public GameObject game;
+    public GameObject enemyGenerator;
+    public AudioClip saltaClip;
+    public AudioClip muereClip;
+    public AudioClip puntoClip;
+    public ParticleSystem dust;
+    private Animator animator;
+    private AudioSource audioPlayer;
+    private float startY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        audioPlayer = GetComponent<AudioSource>();
+        startY = transform.position.y;
+    }
+    // Update is called once per frame
+    void Update()
+    {   //controla que muera el jugador solo si esta en play
+        bool gamePlaying = game.GetComponent<JuegoControl>().gameState == GameState.Playing;
+        bool isGraunded = transform.position.y == startY;
+        bool userAction = Input.GetKeyDown("up") || Input.GetMouseButtonDown(0);
+       
+        //detecta el choque contra un enemigo
+       if(isGraunded && gamePlaying && userAction){
+           UpdateState("Player_Saltar");
+           audioPlayer.clip = saltaClip;
+           audioPlayer.Play();
+       }
+    }
+
+    public void UpdateState(string state = null){
+        if(state != null){
+            animator.Play(state);
+        }
+    }
+    //detecta colicion entre jugador y enemigo y actua en consecuencia
+    void OnTriggerEnter2D(Collider2D other){
+        if(other.gameObject.tag == "Enemy"){
+           UpdateState("Player_Muere");
+           game.GetComponent<JuegoControl>().gameState = GameState.Fin;
+           enemyGenerator.SendMessage("CancelGenerator", true);
+           game.SendMessage("ResetTimeScale", 0.5f);
+          
+           //ref audio
+           game.GetComponent<AudioSource>().Stop();
+           audioPlayer.clip = muereClip;
+           audioPlayer.Play();
+
+            DustStop();
+        } 
+        else if(other.gameObject.tag == "Point"){
+           game.SendMessage("IncreasePoints"); 
+           audioPlayer.clip = puntoClip;
+           audioPlayer.Play();
+        }
+    } 
+
+    void GameReplay(){
+       game.GetComponent<JuegoControl>().gameState = GameState.Replay; 
+    } 
+
+    void DustPlay(){
+        dust.Play();
+    } 
+
+    void DustStop(){
+        dust.Stop();
+    }
+}
